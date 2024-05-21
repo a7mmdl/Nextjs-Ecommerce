9aac81aaa1ed2ad01733d0847963683ae56b09cb
Ahmed <0423rafi@gmail.com>
2024-05-21 18:29:42 +0400

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "cdn.sanity.io**",
+        },
+      ],
+    },
+    env: {
+      NEXT_PUBLIC_STRIPE_KEY: process.env.NEXT_PUBLIC_STRIPE_KEY,
+    },
+  };
+  
+  export default nextConfig;
+  
